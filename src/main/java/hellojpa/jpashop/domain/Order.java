f2b5ee240380dd780ac67dd0d17106135a20fced
Kolin136 <jsowb136@gmail.com>
2024-02-19 16:37:17 +0900

    섹션 5

--- src/main/java/hellojpa/jpashop/domain/Order.java
+++ src/main/java/hellojpa/jpashop/domain/Order.java
@@ -7,8 +7,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 
 @Entity
@@ -20,11 +25,47 @@ public class Order {
   @Column(name = "ORDER_ID")
   private Long id;
 
-  @Column(name = "MEMBER_ID")
-  private Long memberId;
+  @ManyToOne
+  @JoinColumn(name="MEMBER_ID")
+  private Member member;
   private LocalDateTime orderDate;
 
   @Enumerated(EnumType.STRING)
   private OrderStatus status;
 
+  @OneToMany(mappedBy = "order")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setMember(Member member) {
+    this.member = member;
+  }
+
+  public void setOrderDate(LocalDateTime orderDate) {
+    this.orderDate = orderDate;
+  }
+
+  public void setStatus(OrderStatus status) {
+    this.status = status;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public Member getMember() {
+    return member;
+  }
+
+  public LocalDateTime getOrderDate() {
+    return orderDate;
+  }
+
+  public OrderStatus getStatus() {
+    return status;
+  }
 }
--- src/main/java/hellojpa/jpashop/domain/OrderItem.java
+++ src/main/java/hellojpa/jpashop/domain/OrderItem.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class OrderItem {
@@ -14,11 +16,13 @@ public class OrderItem {
   @Column(name = "ORDER_ITEM_ID")
   private Long id;
 
-  @Column(name = "ORDER_ID")
-  private Long orderId;
+  @ManyToOne
+  @JoinColumn(name="ORDER_ID")
+  private Order order;
 
-  @Column(name = "ITEM_ID")
-  private Long itemId;
+  @ManyToOne
+  @JoinColumn(name="ITEM_ID")
+  private Item item;
 
   private int orderPrice;
   private int count;
@@ -27,12 +31,12 @@ public class OrderItem {
     this.id = id;
   }
 
-  public void setOrderId(Long orderId) {
-    this.orderId = orderId;
+  public void setOrder(Order order) {
+    this.order = order;
   }
 
-  public void setItemId(Long itemId) {
-    this.itemId = itemId;
+  public void setItem(Item item) {
+    this.item = item;
   }
 
   public void setOrderPrice(int orderPrice) {
@@ -47,12 +51,12 @@ public class OrderItem {
     return id;
   }
 
-  public Long getOrderId() {
-    return orderId;
+  public Order getOrder() {
+    return order;
   }
 
-  public Long getItemId() {
-    return itemId;
+  public Item getItem() {
+    return item;
   }
 
   public int getOrderPrice() {
